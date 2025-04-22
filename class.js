5b0542e353cdaa85303dbd6fea887c151d412ee5
Roja <kasularoja67@gmail.com>
2025-04-22 14:14:39 -0500

    Inheritance

--- class.js
+++ class.js
@@ -1,6 +1,6 @@
 class Vehicle {
-    constructor(vehicleType, make, model, year, mileage = 0) {
-      this.vehicleType = vehicleType;
+    constructor(type, make, model, year, mileage = 0) {
+      this.type = type;
       this.make = make;
       this.model = model;
       this.year = year;
